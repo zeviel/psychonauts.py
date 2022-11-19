b38d4f064c7c1e1249906f4ed9d11f8c5bd2e7b3
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 19:37:44 +0800

    Update psychonauts.py

--- src/psychonauts.py
+++ src/psychonauts.py
@@ -1,35 +1,33 @@
 from requests import get
 
-
 class Psychonauts:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://psychonauts-api.herokuapp.com/api"
 		self.headers = {
-			"User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
+			"user-agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
 		}
 	
-	def get_all_characters(self, limit: int = 10):
+	def get_all_characters(self, limit: int = 10) -> dict:
 		return get(
 			f"{self.api}/characters?limit={limit}",
 			headers=self.headers).json()
 		
-	def get_character_by_name(self, name: str):
+	def get_character_by_name(self, name: str) -> dict:
 		return get(
 			f"{self.api}/characters?name={name}",
 			headers=self.headers).json()
 	
-	def get_character_by_gender(self, gender: str):
+	def get_character_by_gender(self, gender: str) -> dict:
 		return get(
 			f"{self.api}/characters?gender={gender}",
 			headers=self.headers).json()
 		
-	def get_all_psi_powers(self, limit: int = 10):
+	def get_all_psi_powers(self, limit: int = 10) -> dict:
 		return get(
 			f"{self.api}/powers?limit={limit}",
 			headers=self.headers).json()
 	
-	def get_psi_power_by_name(self, name: str):
+	def get_psi_power_by_name(self, name: str) -> dict:
 		return get(
 			f"{self.api}/powers?name={name}",
 			headers=self.headers).json()
-			
